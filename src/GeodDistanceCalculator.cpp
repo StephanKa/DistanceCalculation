b894b065d5db0b27efad87849b82b66ac311b052
Stephan Kantelberg <stephan.kantelberg@zuehlke.com>
2021-09-01 21:37:14 +0200

    disabled unix part

--- src/GeodDistanceCalculator.cpp
+++ src/GeodDistanceCalculator.cpp
@@ -1,8 +1,3 @@
-#ifndef _WIN32
-#include <stdlib.h>
-#include <unistd.h>
-#endif
-
 #include "GeodDistanceCalculator.h"
 
 /*
@@ -12,40 +7,6 @@
 double GeodDistanceCalculator::distance(Point& from, Point& to) const
 {
 #ifndef _WIN32
-    /*stringstream pin, pout;
-    string s1, s2;
-    double dist;
-    char outbuf[256];
-    int fdin[2], fdout[2];
-
-    pipe(fdin);
-    pipe(fdout);
-    if (0 == fork())
-    {
-        /* child */
-        dup2(fdin[0], 0);
-        close(fdin[1]);
-        dup2(fdout[1], 1);
-        close(fdout[0]);
-        execlp("/usr/bin/geod", "geod", "+ellps=WGS84", "+units=km", "-I", (char*) NULL);
-        cerr << "execlp geod failed" << endl;
-        exit(1);
-    }
-    /* parent */
-    close(fdin[0]);
-    close(fdout[1]);
-
-    pin << from.lat() << " " << from.lon() << " " << to.lat() << " " << to.lon() << endl;
-    write(fdin[1], pin.str().c_str(), pin.str().length());
-    close(fdin[1]);
-
-    read(fdout[0], outbuf, 255);
-    close(fdout[0]);
-
-    pout << outbuf;
-    pout >> s1 >> s2 >> dist;
-
-    return dist;*/
     static_cast<void>(to);
     static_cast<void>(from);
     return -1;
